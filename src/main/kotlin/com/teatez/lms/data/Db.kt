9318f159cb8bfe661ce5d4bc5a597c8279eb6cc5
Charles Teaford <charles.teaford@gmail.com>
2019-06-20 18:35:30 -0400

    adding error impls

--- src/main/kotlin/com/teatez/lms/data/Db.kt
+++ src/main/kotlin/com/teatez/lms/data/Db.kt
@@ -13,21 +13,22 @@ interface Persistable
 interface Persistor<T: Persistable>{
     val db: Db
     fun create(x: T): DbResponse<T, DbCreateError>
-    fun read(query: T): List<T>
-    fun update(query: T): T
-    fun delete(id: T): Boolean
-}
-data class DbCreateError(val c: String, val m: String) : DbError {
-    override fun getCode(): String = c
-    override fun getMessage(): String = m
+    fun read(query: T): DbResponse<List<T>, DbReadError>
+    fun update(query: T): DbResponse<T, DbUpdateError>
+    fun delete(id: T): DbResponse<Boolean, DbDeleteError>
 }
 
-interface DbResponse<S: Persistable,F: DbError>
-data class Success<S: Persistable, F: DbError>(val value: S): DbResponse<S,F>
-data class Failure<S: Persistable, F: DbError>(val error: F): DbResponse<S,F>
+data class DbCreateError(override val code: String, override val message: String) : DbError
+data class DbReadError(override val code: String, override val message: String) : DbError 
+data class DbUpdateError(override val code: String, override val message: String) : DbError 
+data class DbDeleteError(override val code: String, override val message: String) : DbError
+
+interface DbResponse<S,F: DbError>
+data class Success<S, F: DbError>(val value: S): DbResponse<S,F>
+data class Failure<S, F: DbError>(val error: F): DbResponse<S,F>
 
 interface DbError {
-    fun getCode(): String
-    fun getMessage(): String
+    val code: String
+    val message: String
 }
 data class Butthole(val shit: String): Persistable
